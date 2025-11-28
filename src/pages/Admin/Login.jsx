b060fa9c51c738835e13adbd07b6365cbc281b30
dilshad <dilshadiits@gmail.com>
2025-11-29 00:21:34 +0530

    tim

--- src/pages/Admin/Login.jsx
+++ src/pages/Admin/Login.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import API_BASE from './api';
-
+import API_BASE from '../../api';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
