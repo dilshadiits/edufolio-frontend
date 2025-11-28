dc993f0e0e7ce5650bfb260cab0e6d1cb762d5bf
dilshad <dilshadiits@gmail.com>
2025-11-28 23:30:45 +0530

    fixed admin import paths

--- src/pages/Admin/Login.jsx
+++ src/pages/Admin/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import API_BASE from '../../api';
+import API_BASE from './api';
 
 const Login = () => {
     const [email, setEmail] = useState('');
---
+++ src/pages/Admin/api.js
@@ -0,0 +1,2 @@
+const API_BASE = "https://edufolio-backend-w2yi.onrender.com/api";
+export default API_BASE;
